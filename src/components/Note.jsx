6837700883c12ffe85d7cce9c43aff8d4308dfcd
Uma Negi <umanegi267@gmail.com>
2020-09-24 09:22:05 +0100

    First commit: create app,header,footer,note components

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note(){
+
+    return(
+        <div class="note">
+           <h1>This is the heading </h1>
+           <p> This is the content </p>
+        </div>
+  
+    );
+};
+
+export default Note;
